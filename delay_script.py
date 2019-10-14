0bd34d105bb6695aee7b0efdfd0250a5a82894cc
M. Sutphin <55960133+msutphin@users.noreply.github.com>
2019-10-14 10:10:24 -0400

    Create delay_script.py

---
+++ delay_script.py
@@ -0,0 +1,11 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+device = {
+  "host": 'nxos2.lasthop.io'
+  "username": 'pyclass'
+  "password": getpass()
+  "device_type": 'cisco_nxos_ssh'
+  }
+  
+  
