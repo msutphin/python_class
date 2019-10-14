2080a5f219494af79bd3507d7376a45fb1d266a2
M. Sutphin <55960133+msutphin@users.noreply.github.com>
2019-10-14 11:26:56 -0400

    Update delay_script.py

--- delay_script.py
+++ delay_script.py
@@ -2,10 +2,15 @@ from netmiko import ConnectHandler
 from getpass import getpass
 
 device = {
-  "host": 'nxos2.lasthop.io'
-  "username": 'pyclass'
-  "password": getpass()
-  "device_type": 'cisco_nxos_ssh'
+  "host": 'nxos2.lasthop.io',
+  "username": 'pyclass',
+  "password": getpass(),
+  "device_type": 'cisco_nxos_ssh',
+  "global_delay_factor": 2,
   }
   
+  net_connect = ConnectHandler(**device)
+  print(net_connect.find_prompt())
   
+  output = net_connect.send_command("show lldp neighbors detail")
+  print(output)
